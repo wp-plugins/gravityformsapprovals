6d5dd9e611a9f6865f032c05d516136e2730c746
stevehenty <stevehenty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-27 18:16:50 +0000

    Bumping version to 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/gravityformsapprovals/trunk@1254579 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- approvals.php
+++ approvals.php
@@ -3,7 +3,7 @@
 Plugin Name: Gravity Forms Approvals
 Plugin URI: http://www.stevenhenty.com
 Description: Add simple approvals to Gravity Forms
-Version: 1.0-beta-5
+Version: 1.0.0
 Author: Steve Henty
 Author URI: http://www.stevenhenty.com
 License: GPL-2.0+
@@ -27,7 +27,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define( 'GF_APPROVALS_VERSION', '1.0-beta-5' );
+define( 'GF_APPROVALS_VERSION', '1.0.0' );
 
 add_action( 'gform_loaded', array( 'GF_Approvals_Bootstrap', 'load' ), 5 );
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.stevenhenty.com/products/wordpress-plugins/donate
 Tags: gravity forms, approvals, workflow
 Requires at least: 4.0
 Tested up to: 4.3
-Stable tag: 1.0-beta-4
+Stable tag: 1.0.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -66,6 +66,9 @@ Ensure your role is assigned the gravityformsapprovals_form_settings capability 
 
 
 == ChangeLog ==
+
+= 1.0.0 =
+- Bumping version
 
 = 1.0-beta-5 =
 - Fixed an issue where User Registration is skipped even when there are no approval feeds.
